722811106f1ca17d6c3b639d1c58a77cb972dea4
SchelZ <67759165+SchelZ@users.noreply.github.com>
2024-04-09 03:01:44 +0300

    Add files via upload

---
+++ dumper.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import urllib.request, os, requests
+from PIL import Image
+from io import BytesIO
+
+name = "bayonet"
+opener=urllib.request.build_opener()
+opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+urllib.request.install_opener(opener)
+
+req = urllib.request.Request(
+    url=f'https://csgoskins.gg/weapons/{name}', 
+    headers={'User-Agent': 'Mozilla/5.0'}
+)
+webpage = urllib.request.urlopen(req).read()
+
+soup = BeautifulSoup(webpage, features="html5lib")
+
+
+size = (150, 115)
+dir = os.path.dirname(os.path.realpath(__file__))
+for each_div in soup.findAll('img',{'class':'mx-auto'}):
+    data = requests.get(each_div['src'], headers={'User-Agent': 'Mozilla/5.0'}).content
+    pilImage = Image.open(BytesIO(data)).convert('RGBA')
+    img = pilImage.resize(size, resample=Image.BICUBIC)
+    img.save(f"{dir}\{name}_{each_div['alt'].split(' ')[-1]}.png", optimize=True, quality=30)
