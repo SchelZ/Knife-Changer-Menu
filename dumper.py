6343095d92f5655a7c181c73800531efd97a5a9d
SchelZ <67759165+SchelZ@users.noreply.github.com>
2024-04-09 03:03:56 +0300

    Update dumper.py

--- dumper.py
+++ dumper.py
@@ -1,23 +1,18 @@
-from bs4 import BeautifulSoup
 import urllib.request, os, requests
+from bs4 import BeautifulSoup
 from PIL import Image
 from io import BytesIO
 
-name = "bayonet"
+name = "bayonet"        ## change based on desired weapon
 opener=urllib.request.build_opener()
 opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
 urllib.request.install_opener(opener)
 
-req = urllib.request.Request(
-    url=f'https://csgoskins.gg/weapons/{name}', 
-    headers={'User-Agent': 'Mozilla/5.0'}
-)
+req = urllib.request.Request(url=f'https://csgoskins.gg/weapons/{name}')
 webpage = urllib.request.urlopen(req).read()
-
 soup = BeautifulSoup(webpage, features="html5lib")
 
-
-size = (150, 115)
+size = (150, 115)        ## image ouput resolution
 dir = os.path.dirname(os.path.realpath(__file__))
 for each_div in soup.findAll('img',{'class':'mx-auto'}):
     data = requests.get(each_div['src'], headers={'User-Agent': 'Mozilla/5.0'}).content
